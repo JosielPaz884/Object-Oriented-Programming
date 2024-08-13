217072adcf7107f937e6a1ddd89bd71b225c1866
Josiel <105311011+JosielPaz884@users.noreply.github.com>
2024-08-13 20:41:42 -0300

    Update Area.java

--- Lista 1/Area.java
+++ Lista 1/Area.java
@@ -1,1 +1,15 @@
+//Questão 1
+import java.util.Scanner;
 
+public class Area {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a altura do Retângulo:");
+        double altura = scanner.nextDouble();
+        System.out.println("Digite a largura do Retângulo:");
+        double largura = scanner.nextDouble();
+        double area = altura * largura;
+        System.out.println("A área do retângulo é: " + area);
+        scanner.close();
+    }
+}
