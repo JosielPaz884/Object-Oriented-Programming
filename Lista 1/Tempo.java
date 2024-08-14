67703f11d166d60c1be3aa911084e92364b62a5b
Josiel <105311011+JosielPaz884@users.noreply.github.com>
2024-08-14 19:04:32 -0300

    Update Tempo.java

--- Lista 1/Tempo.java
+++ Lista 1/Tempo.java
@@ -4,29 +4,29 @@ import java.util.Scanner;
 public class Tempo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite a hora do primeiro horário:");
-        int hora1 = readNumber(scanner);
-        System.out.println("Digite a minuto do primeiro horário:");
-        int minuto1 = readNumber(scanner);
-        System.out.println("Digite a segundo do primeiro horário:");
-        int segundo1 = readNumber(scanner);
-        System.out.println("Primeiro Horário" + hora1 + ":" + minuto1 + ":" + segundo1);
-        System.out.println("Digite a hora do segundo horário:");
-        int hora2 = readNumber(scanner);
-        System.out.println("Digite a minuto do segundo horário:");
-        int minuto2 = readNumber(scanner);
-        System.out.println("Digite a segundo do segundo horário:");
-        int segundo2 = readNumber(scanner);
-        System.out.println("Segundo Horário" + hora2 + ":" + minuto2 + ":" + segundo2);
-        int convertSegundo1 = (hora1 * 3600) + (minuto1 * 60) + segundo1;
-        int convertSegundo2 = (hora2 * 3600) + (minuto2 * 60) + segundo2;
-        int sub = Math.abs(convertSegundo2 - convertSegundo1);
+        int[] time1 = readNumber(scanner);
+        System.out.println("Primeiro Horário: " + time1[0] + ":" + time1[1] + ":" + time1[2]);
+        int[] time2 = readNumber(scanner);
+        System.out.println("Segundo Horário: " + time2[0] + ":" + time2[1] + ":" + time2[2]);
+        int sub = convert(time1, time2);
         System.out.println("A diferença entre os dois horários em segundo é: " + sub + " s");
         scanner.close();
     }
 
-    private static int readNumber(Scanner scanner) {
-        int number = scanner.nextInt();
-        return number;
+    private static int[] readNumber(Scanner scanner) {
+        System.out.println("Digite a hora do horário:");
+        int hora = scanner.nextInt();
+        System.out.println("Digite a minuto do horário:");
+        int minuto = scanner.nextInt();
+        System.out.println("Digite a segundo do horário:");
+        int segundo = scanner.nextInt();
+        return new int[] { hora, minuto, segundo };
+    }
+
+    private static int convert(int[] time1, int[] time2) {
+        int convertSegundo1 = (time1[0] * 3600) + (time1[1] * 60) + time1[2];
+        int convertSegundo2 = (time2[0] * 3600) + (time2[1] * 60) + time2[2];
+        int sub = Math.abs(convertSegundo2 - convertSegundo1);
+        return sub;
     }
 }
