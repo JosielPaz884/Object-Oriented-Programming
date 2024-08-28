12235b93cdc62bb0856d2a1b551101f809a15590
Josiel <105311011+JosielPaz884@users.noreply.github.com>
2024-08-28 13:05:41 -0300

    Update Carro.java

--- Lista 3/Exercício 6/Carro.java
+++ Lista 3/Exercício 6/Carro.java
@@ -4,7 +4,6 @@ public class Carro {
     private int year;
     private double mileage;
 
-    // Construtor
     public Carro(String mark, String model, int year, double mileage) {
         this.mark = mark;
         this.model = model;
@@ -12,23 +11,23 @@ public class Carro {
         this.mileage = mileage;
     }
 
-    // Método para alterar a quilometragem total
     public void setMileage(double mileage) {
         this.mileage = mileage;
     }
 
-    // Método sobrecarregado para alterar a quilometragem percorrida em uma viagem
-    public void setMileageTraveled(double mileageTraveled) {
-        this.mileageTraveled += mileageTraveled;
+    public void setMileageTraveled(double mileageTraveled, boolean travel) {
+        if(travel){
+             this.mileageTraveled += mileageTraveled;
+        }else{
+             this.mileageTraveled = mileageTraveled;
+        }
     }
-
-    // Método para obter a quilometragem atual
+    
     public double getMileage() {
         return this.Mileage;
     }
-
-    // Método para imprimir informações do carro
-    public void imprimirInformacoes() {
+    
+    public void printInformation() {
         System.out.println("Marca: " + this.mark);
         System.out.println("Modelo: " + this.model);
         System.out.println("Ano: " + this.year);
