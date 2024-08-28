8b0fe479b8219d1d2c0fca03ba3e5d104b66bddc
Josiel <105311011+JosielPaz884@users.noreply.github.com>
2024-08-28 00:35:59 -0300

    Create Disciplina.java

---
+++ Lista 3/Exercício 2/Disciplina.java
@@ -0,0 +1,18 @@
+public class Disciplina {
+    private String name;
+    private double note;
+
+    public Disciplina(String name, double note) {
+        this.name = name;
+        this.note = note;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getNote() {
+        return note;
+    }
+
+}
