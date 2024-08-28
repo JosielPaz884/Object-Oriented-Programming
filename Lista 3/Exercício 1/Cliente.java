92d62e153f37cbd2c8beeb34e23400229403807d
Josiel <105311011+JosielPaz884@users.noreply.github.com>
2024-08-28 00:33:05 -0300

    Create Cliente.java

---
+++ Lista 3/Exercício 1/Cliente.java
@@ -0,0 +1,23 @@
+public class Cliente {
+    private String name;
+    private String cpf;
+    private String address;
+
+    public Cliente(String name, String cpf, String address) {
+        this.name = name;
+        this.cpf = cpf;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
