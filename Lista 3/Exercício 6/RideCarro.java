8f07fd5185c2fc3333b0b9efded503a03bbf81c4
Josiel <105311011+JosielPaz884@users.noreply.github.com>
2024-08-28 13:00:14 -0300

    Update RideCarro.java

--- Lista 3/Exercício 6/RideCarro.java
+++ Lista 3/Exercício 6/RideCarro.java
@@ -2,14 +2,14 @@ public class RideCarro{
   public static void main(String[] args) {
         Carro myCar = new Carro("Ford", "Focus", 2015, 0);
         System.out.println("Informações iniciais do carro:");
-        myCar.imprimirInformacoes();
+        myCar.printInformation();
 
-        myCar.setKilometragem(10000); 
+        myCar.setMileage(10000); 
         System.out.println("\nInformações do carro após alterar a quilometragem total:");
         myCar.imprimirInformacoes();
 
-        myCar.setKilometragem(500, true); 
+        myCar.setMileageTraveled(500, true); 
         System.out.println("\nInformações do carro após somar quilometragem de viagem:");
-        myCar.imprimirInformacoes();
+        myCar.printInformation());
     }
 }
