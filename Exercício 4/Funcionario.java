ac784754363847bdb80eced5303c790705a06e76
Josiel <105311011+JosielPaz884@users.noreply.github.com>
2024-09-04 13:02:44 -0300

    Create Funcionario.java

---
+++ Exercício 4/Funcionario.java
@@ -0,0 +1,53 @@
+public class Funcionario {
+    private int code;
+    private double quantityHours;
+    private double valueTime;
+    private double valueSalary;
+    private double amountOvertime;
+
+    public Funcionario(int code, double quantityHours, double valueTime) {
+        this.code = code;
+        this.quantityHours = quantityHours;
+        this.valueTime = valueTime;
+        this.valueSalary = 0; 
+        this.amountOvertime = 0; 
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public double getQuantityHours() {
+        return quantityHours;
+    }
+
+    public double getValueTime() {
+        return valueTime;
+    }
+
+    public double getValueSalary() {
+        return valueSalary;
+    }
+
+    public double getAmountOvertime() {
+        return AmountOvertime;
+    }
+
+    public void setAmountOvertime(double amountOvertime) {
+        this.amountOvertime = amountOvertime;
+    }
+
+    public double calculateOvertime() {
+        return amountOvertime * valueTime * 1.5; 
+    }
+
+    public void calculateSalary() {
+       valueSalary = quantityHours *valueTime +calculateOvertime();
+    }
+
+    public void print() {
+        System.out.println("Código: " + code);
+        System.out.println("Salário: R$ " + valueSalary);
+        System.out.println("Quantidade de horas trabalhadas: " + quantityHours);
+    }
+}
