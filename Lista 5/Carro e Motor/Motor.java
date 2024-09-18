ec96671f9959f46b192ed73f58b22f558c30539e
Josiel <105311011+JosielPaz884@users.noreply.github.com>
2024-09-18 01:16:08 -0300

    Create Motor.java

---
+++ Lista 5/Carro e Motor/Motor.java
@@ -0,0 +1,20 @@
+public class Motor {
+    private double potencia;
+    private String tipoCombustivel;
+
+    public void setPotencia(double potencia) {
+        this.potencia = potencia;
+    }
+
+    public void setTipoCombustivel(String tipoCombustivel) {
+        this.tipoCombustivel = tipoCombustivel;
+    }
+
+    public double getPotencia() {
+        return potencia;
+    }
+
+    public String getTipoCombustivel() {
+        return tipoCombustivel;
+    }
+}
