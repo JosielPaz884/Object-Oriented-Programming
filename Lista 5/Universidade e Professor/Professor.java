e25a5ddf2938a77fe0917ae9a0425896db7cfd03
Josiel <105311011+JosielPaz884@users.noreply.github.com>
2024-09-18 01:17:46 -0300

    Create Professor.java

---
+++ Lista 5/Universidade e Professor/Professor.java
@@ -0,0 +1,17 @@
+public class Professor {
+    private String name;
+    private String subject;
+
+    public Professor(String name, String subject) {
+        this.name = name;
+        this.subject = subject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+}
